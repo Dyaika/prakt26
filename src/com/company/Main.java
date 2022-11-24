2d709ff64832357ae994dc91b98b18021963639f
Dyaika <91870391+Dyaika@users.noreply.github.com>
2022-11-24 17:09:42 +0300

    ПРактическая 26

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+    public static void exceptionDemo2(){
+        try{
+            System.out.println(2/0);
+        } catch (ArithmeticException e){
+            System.out.println("Attempted division by zero.");
+        }
+    }
+    public static void exceptionDemo1(){
+        System.out.println(2.0/0.0);
+    }
+    public static void main(String[] args) {
+        exceptionDemo1();
+        exceptionDemo2();
+    }
+}
